d2b698d13da4d69a52c4416fd500e322363a8565
EC2 Default User <ec2-user@ip-172-31-43-1.ec2.internal>
2021-02-07 03:14:56 +0000

    Second commit, added in code to use spotif api to fetch song info from a randomly selected artist, added in basic display info using flask and html

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template
+import random
 
 load_dotenv(find_dotenv())
 
@@ -28,19 +29,51 @@ headers = {
 
 app = Flask(__name__)
 
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+artist_list = ["6XyY86QOPPrYVGvF9ch6wz","72X6FHxaShda0XeQw3vbeF","3o2dn2O0FCVsWDFSh8qxgG"]
+
+rand_select = random.randint(0,2)
+
+artist_select = artist_list[rand_select]
+
+artist_url = 'https://api.spotify.com/v1/artists/'
+
+artist_param = {
+    'market': 'US'
+}
+
+artist_request = requests.get(artist_url + artist_select + '/top-tracks', headers = headers, params = artist_param)
+
+artist_data = artist_request.json()
+
+song_select = random.randint(0,5)
+
+song_name = artist_data['tracks'][song_select]['name']
+print(song_name)
+song_artist = artist_data['tracks'][song_select]['artists'][0]['name']
+print(song_artist)
+song_pic = artist_data['tracks'][song_select]['album']['images'][0]['url']
+print(song_pic)
+song_url = artist_data['tracks'][song_select]['preview_url']
+print(song_url)
+
 @app.route('/')
 def Spotify_Display():
     print("test")
     return render_template(
         "index.html", 
+        name = song_name,
+        artist = song_artist,
+        preview = song_url,
+        picture = song_pic
     )
     
 app.run(
     port=int(os.getenv('PORT', 8080)),
-    debug=True
+    #debug=True
 )
-
-
 
 
 
